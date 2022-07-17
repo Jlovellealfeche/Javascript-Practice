aa514cec0b2c50ab7ac9995d3448004d6eeec60a
joan lovelle alfeche <jlovellealfeche@gmail.com>
2022-07-17 17:16:18 +0200

    Update app.js

--- setup/app.js
+++ setup/app.js
@@ -1,1 +1,7 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector("color")       //This is for my color span 
+
+btn.addEventListener('click', function(){
+ console.log(document.body);
+}); //Passed a acallback function 
