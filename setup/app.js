257a21b1ef29ca92f76d8d89d2dab58c3cde26c5
joan lovelle alfeche <jlovellealfeche@gmail.com>
2022-07-17 19:18:33 +0200

    applying math floor
    To round up to the closest integer

--- setup/app.js
+++ setup/app.js
@@ -1,10 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");  //Target for button
-const color = document.querySelector("color")       //This is for my color span 
+const color = document.querySelector(".color");       //This is for my color span 
 
 btn.addEventListener("click", function () {
  //get random number between 0-3 colors [3] -->This is the goal
- const randomNumber = 2;
+ const randomNumber = getRandomNumber();
+ console.log(randomNumber);
+
  document.body.style.backgroundColor = colors[randomNumber];
- color.textContent  = colors[randomNumber];
+ color.textContent = colors[randomNumber];
 }); //Passed a callback function 
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
