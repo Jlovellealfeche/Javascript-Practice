3952ff839f166cc8f61974a7d739e932a40dede8
joan lovelle alfeche <jlovellealfeche@gmail.com>
2022-07-17 17:33:40 +0200

    .

--- setup/app.js
+++ setup/app.js
@@ -1,7 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn");  //Target for button
 const color = document.querySelector("color")       //This is for my color span 
 
-btn.addEventListener('click', function(){
- console.log(document.body);
-}); //Passed a acallback function 
+btn.addEventListener("click", function () {
+ //get random number between 0-3 colors [3] -->This is the goal
+ const randomNumber = 2;
+ document.body.style.backgroundColor = colors[randomNumber];
+ color.textContent  = colors[randomNumber];
+}); //Passed a callback function 
