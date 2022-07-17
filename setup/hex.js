d9486510a346810c8b1c92230a7bc70f4f3778a1
joan lovelle alfeche <jlovellealfeche@gmail.com>
2022-07-17 19:46:24 +0200

    Update hex.js

--- setup/hex.js
+++ setup/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+//letters will represent values 10, 11,12,13, 14.15
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+let hexColor = "#";
+for(let i = 0;  i < 6;  i++) {
+    hexColor += hex[getRandomNumber()];
+}
+color.textContent = hexColor;
+document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+} ///Mutiply it with the hex length of the array
